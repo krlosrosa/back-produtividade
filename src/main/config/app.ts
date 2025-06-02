99e7dddffc6d56436e7d8dafa07eeef0fc207fb9
krlosrosa <robertorsilva.krlos@gmail.com>
2025-06-02 12:23:08 -0300

    Update app.ts

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,6 @@
 import express, { type Express } from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
-import dotenvFlow from 'dotenv-flow'
 
 dotenvFlow.config()
 
